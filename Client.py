a656cd46c3cee63cb6d378ab2cfe27efb1ed1457
hhive <31347477+hhive@users.noreply.github.com>
2018-07-29 23:53:12 +0800

    接收多条消息

--- Client.py
+++ Client.py
@@ -1,67 +1,127 @@
 #!/usr/bin/python3
 # 文件名：client.py
- 
+
 # 导入 socket、sys 模块
 import socket
 import sys
 import time
- 
- #自动重连
-def doConnect(host,port):
-	# 创建 socket 对象
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try :
-    	# 连接服务，指定主机和端口
-        sock.connect((host,port))
-    except :
-        pass
-    return sock
+import threading
 
-def main():
-	#s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) #在客户端开启心跳维护
-	#s.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
-	buffer = []
-	# 获取本地主机名
-	host = socket.gethostname()
-	#host = 'www.sina.com.cn'
-	# 设置端口
-	port = 9996
-	s = doConnect(host,port)
-	s.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
-	while True:
-		try:
-			# 接收小于 1024 字节的数据
-			hello = 'Hello'+ "\r\n"
-			s.send(hello.encode('utf-8'))
-			#读取接收到的数据并写入文件
-			print("Client开始读入数据")
-			msg = s.recv(1024)   #接收小于1024字节的数据
-			print(msg)
-			if msg:
-				buffer.append(msg.decode('utf-8'))
-			else:
-				print("Client数据读取完成")#？
-				break
-			print("Client开始写入文件")
-			data = ''.join(buffer)
-			with open('Client.txt', 'a') as f:
-    				f.write(data)
-			print (msg.decode('utf-8'))
 
-		except socket.error:
-			print ('\r\nsocket error,do reconnect')
-			#推迟执行
-			time.sleep(3)
-			#自动重连
-			s = doConnect(host,port)
+class Client:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    isOk = True
 
-		except IOError:
-			print('\r\nwrite error')
-			time.sleep(3)
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.s = self.do_connect(self.host, self.port)
+        self.s.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
+        print("正在初始化")
 
-		except:
-			print ('\r\nother error occur ')
-		time.sleep(1)
+    # 自动重连
+    def do_connect(self, host, port):
+        # 创建 socket 对象
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            # 连接服务，指定主机和端口
+            sock.connect((host, port))
+            self.isOk = True
+        except:
+            pass
+        return sock
 
-if __name__ == "__main__":
-	main()
+    def send_msg(self):
+        #global s
+        while True:
+            try:
+
+                if self.isOk:
+                    print("请输入你想发送的数据，回车结束：")
+                    # hello = 'Hello' + "\r\n"
+                    hello = input()
+                    hello = hello + "\r\n"
+                self.s.send(hello.encode('utf-8'))
+
+            except socket.error:
+                self.isOk = False
+                print('\r\n网络错误，开始重连')
+                # 推迟执行
+                time.sleep(3)
+                # 自动重连
+                self.s = self.do_connect(self.host, self.port)
+
+    def recever_msg(self):
+        #global s
+        End = "\r\n"
+        buffer = []
+        while True:
+            try:
+                total_data = []
+                data = ''
+                while True:
+                    data = socket.recv(8192)
+                    if End in data:
+                        total_data.append(data[:data.find(End)])
+                        break
+                    total_data.append(data)
+                    if len(total_data) > 1:
+                        # check if end_of_data was split
+                        last_pair = total_data[-2] + total_data[-1]
+                        if End in last_pair:
+                            total_data[-2] = last_pair[:last_pair.find(End)]
+                            total_data.pop()
+                            break
+                print("Client开始写入文件")
+                data = ''.join(buffer)  # join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串。
+                with open('Client.txt', 'a') as f:
+                    f.write(data)
+                #print(msg.decode('utf-8'))
+            except socket.error:
+                self.isOk = False
+                print('\r\n网络错误，开始重连')
+                # 推迟执行
+                time.sleep(3)
+                # 自动重连
+                self.s = self.do_connect(self.host, self.port)
+
+            except IOError:
+                print('\r\n写入文件错误')
+                time.sleep(3)
+
+            except:
+                print('\r\n其他错误发生')
+            time.sleep(1)
+
+    def player(self, name):
+        if name == 'send':
+            self.send_msg()
+        else:
+            if name == 'recv':
+                self.recever_msg()
+            else:
+                print('error: The format is not recognized!')
+
+
+list = ['send', 'recv']
+sockets = Client(socket.gethostname(), 9996)
+# sockets.send_msg()
+# sockets.recever_msg()
+# t1 = threading.Thread(target=sockets.send_msg())
+# t2 = threading.Thread(target=sockets.recever_msg())
+# t2.start()
+# t1.join()
+files = range(len(list))
+threads = []
+
+# 创建线程
+for i in files:
+    t = threading.Thread(target=sockets.player, args=(list[i],))
+    threads.append(t)
+
+if __name__ == '__main__':
+    # 启动线程
+    for i in files:
+        threads[i].start()
+    for i in files:
+        threads[i].join()
--- Server.py
+++ Server.py
@@ -1,40 +1,43 @@
-#!/usr/bin/python3
+# coding=utf-8
+# !/usr/bin/python3
 # 文件名：server.py
- 
+
 # 导入 socket、sys 模块
 import socket
 import sys
 import time
- 
+
 # 创建 socket 对象
-serversocket = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)
- 
+server_socket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+
 # 获取本地主机名
 host = socket.gethostname()
- 
-port = 9996
- 
+
+port = 9998
+
 # 绑定端口
-serversocket.bind((host, port))
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) #在服务器端开启心跳维护value设置为1，表示将SO_REUSEADDR标记为TRUE，操作系统会在服务器socket被关闭或服务器进程终止后马上释放该服务器的端口，否则操作系统会保留几分钟该端口。
+server_socket.bind((host, port))
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE,
+                         1)  # 在服务器端开启心跳维护value设置为1，表示将SO_REUSEADDR标记为TRUE，操作系统会在服务器socket被关闭或服务器进程终止后马上释放该服务器的端口，否则操作系统会保留几分钟该端口。
 # 设置最大连接数，超过后排队
-serversocket.listen(5)
- 
+server_socket.listen(5)
+
 while True:
     # 建立客户端连接
     print("Server开始连接Client")
-    clientsocket,addr = serversocket.accept() #只连接一次所以只执行一次？
-    #发送数据
+    client_socket, addr = server_socket.accept()  # 只连接一次所以只执行一次？
+    # 发送数据
     print("连接地址: %s" % str(addr))
     print("Server开始发送数据")
-    msg='欢迎访问！'+ "\r\n"
-    clientsocket.send(msg.encode('utf-8'))
-    #接受数据
+    #msg = input() + "\r\n"
+    msg = """运泰利自动化是致力于工业自动化产品的研制和开发、自动测试系统与各类五金模具、工装、测试治具的设计和制作，其自动化设备主要涵盖生产自动化、工厂自动化、过程控制、整体解决方案以及产品测试系统等领域。公司本着“满足客户需求，超越客户期望”的宗旨、“服务至上，质量第一”的原则，定位于为客户提供“高可靠、高品质的产品、最称心的服务”为目标，按客户的设备和工艺要求进行自动化系统的设计、安装和调试。提供自动化解决方案。
+    运泰利自动化目前拥有各类进口和国产数控机床及专用设备30余台，汇集了一批国内优秀的产品研发专才、工程技术人员和服务队伍。其中中高级技术人员占百分之六十。并于2008年通过ISO9001：2000质量管理体系的认证。严格的管理制度保证了产品的高品质、高质量。以精湛的专业技术力量，为每一台出厂的设备提供最佳的品质保障。"""
+    msg = msg + "\r\n"
+    client_socket.send(msg.encode())
+    # 接受数据
     print("Server开始接收数据")
-    get = clientsocket.recv(1024)
+    get = client_socket.recv(1024)
     print(get.decode('utf-8'))
 
     time.sleep(3)
-
-    #clientsocket.close()
